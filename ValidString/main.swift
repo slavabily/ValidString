baa5e43b48cf2b764c2b905546b39cf81257029e
slava bily <slavabily@me.com>
2021-11-26 22:28:37 +0200

    2 tests from 3 are running successfully

--- ValidString/main.swift
+++ ValidString/main.swift
@@ -13,10 +13,21 @@ func isValid(s: String) -> String {
     print(mappedItems)
     let counts = Dictionary(mappedItems) { $0 + $1 }
     print(counts)
-    
-    
+    // assessment of character appearing freqwency
+    var freqs = 0
+    var diff = 0
+    for (_, freq) in counts {
+        if freq != freqs {
+            freqs = freq
+            diff += 1
+        }
+    }
+    print(diff)
+    if diff > 2 {
+        return "NO"
+    }
     return "YES"
 }
 
-print(isValid(s: "aabbcd"))
+print(isValid(s: "abcdefghhgfedecba"))
 
